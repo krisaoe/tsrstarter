ac7099bc2cb2a364518227b4bedf9e08356c67e5
krisaoe <powellk@utas.edu.au>
2017-02-21 15:18:18 +1100

    complete for now. no hot-reload yet

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello Universe!</div>, document.getElementById('root'));
+ReactDOM.render(<div>Hello Universe!!</div>, document.getElementById('root'));
